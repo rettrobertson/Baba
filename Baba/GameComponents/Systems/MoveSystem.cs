853a01860e602d6b7c39a3e1b519e87a532ee167
Tyler Totsch <utettotsch@gmail.com>
2023-04-21 11:01:51 -0600

    Pushing almost works

--- Baba/GameComponents/Systems/MoveSystem.cs
+++ Baba/GameComponents/Systems/MoveSystem.cs
@@ -9,14 +9,16 @@ namespace Baba.GameComponents.Systems
     {
         List<Entity> controlledEntities;
         List<Entity> pushableEntities;
-        private bool[,] hittables;
+        List<Entity> wallEntities;
+        private Entity[,] hittables;
         
 
-        public MoveSystem(NewGameView view) : base(view, typeof(You), typeof(Push))
+        public MoveSystem(NewGameView view) : base(view, typeof(You), typeof(Push), typeof(Stop))
         {
             controlledEntities = new List<Entity>();
             pushableEntities = new List<Entity>();
-            hittables = new bool[20,20];
+            wallEntities = new List<Entity>();
+            hittables = new Entity[20,20];
         }
 
         protected override void EntityChanged(Entity entity, Component component, Entity.ComponentChange change)
@@ -35,20 +37,27 @@ namespace Baba.GameComponents.Systems
 
         public override void Update(GameTime gameTime)
         {
-            for (int i = 0; i < controlledEntities.Count; i++)
-            {
-                Vector2 controlled = controlledEntities[i].transform.position;
-                hittables[(int)controlled.Y,(int)controlled.X] = true;
-            }
-            for (int i = 0; i < pushableEntities.Count; i++)
-            {
-                Vector2 controlled = pushableEntities[i].transform.position;
-                hittables[(int)controlled.Y,(int)controlled.X] = true;
-            }
+            
         }
        
         public void moveEntity( GameTime gameTime, string command)
         {
+            for (int i = 0; i < controlledEntities.Count; i++)
+            {
+                Vector2 controlled = controlledEntities[i].transform.position;
+                hittables[(int)controlled.Y, (int)controlled.X] = controlledEntities[i];
+            }
+            for (int i = 0; i < pushableEntities.Count; i++)
+            {
+                Vector2 controlled = pushableEntities[i].transform.position;
+                hittables[(int)controlled.Y, (int)controlled.X] = pushableEntities[i];
+            }
+            for (int i = 0; i < wallEntities.Count; i++)
+            {
+                Vector2 controlled = pushableEntities[i].transform.position;
+                hittables[(int)controlled.Y, (int)controlled.X] = wallEntities[i];
+
+            }
             foreach (Entity entity in controlledEntities)
             {
                 Vector2 currPos = entity.transform.position;
@@ -77,6 +86,10 @@ namespace Baba.GameComponents.Systems
 
                 if (canMove(newPos, command) && newPos != currPos)
                 {
+                    if (hittables[(int)newPos.Y, (int)newPos.X] != null)
+                    {
+                        move(hittables[(int)newPos.Y, (int)newPos.X], command);
+                    }
                     entity.transform.position = newPos;
                 }
             }
@@ -89,7 +102,7 @@ namespace Baba.GameComponents.Systems
             {
                 return false;
             }
-            if (!hittables[(int)newPos.Y, (int)newPos.X])
+            if (hittables[(int)newPos.Y, (int)newPos.X] == null)
             {
                 return true;
             }
@@ -102,15 +115,44 @@ namespace Baba.GameComponents.Systems
                     case "Down":
                         return canMove(newPos + new Vector2(0, 1), direction);
                     case "Left":
-                        return canMove(newPos + new Vector2(-1, 0), direction);
+                        return canMove(newPos + new Vector2(-1, 0), direction);;
                     case "Right":
                         return canMove(newPos + new Vector2(1, 0), direction);
                 }
             }
             return false;
         }
-       
+        private void move(Entity entity, string direction)
+        {
+            Vector2 currPos = entity.transform.position;
+            Vector2 newPos = currPos;
+            switch (direction)
+            {
+                case "Up":
+                    newPos = currPos + new Vector2(0, -1);
 
+
+                    break;
+                case "Down":
+                    newPos = currPos + new Vector2(0, 1);
+
+
+                    break;
+                case "Left":
+                    newPos = currPos + new Vector2(-1, 0);
+
+                    break;
+                case "Right":
+                    newPos = currPos + new Vector2(1, 0);
+                    break;
+
+            }
+            if (hittables[(int)newPos.Y, (int)newPos.X] != null)
+            {
+                move(hittables[(int)newPos.Y, (int)newPos.X], direction);
+            }
+            entity.transform.position = newPos;
+        }
         public override void Reset()
         {
             controlledEntities.Clear();
@@ -119,7 +161,7 @@ namespace Baba.GameComponents.Systems
             {
                 for (int j = 0; j < 20; j++)
                 {
-                    hittables[i, j] = false;
+                    hittables[i, j] = null;
                 }
             }
 

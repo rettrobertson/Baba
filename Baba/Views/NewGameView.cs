5de1a6669b59d2d058c6617a6f2d9556bdfaa949
Tyler Totsch <utettotsch@gmail.com>
2023-04-22 18:29:43 -0600

    Maybe fixed wall bug

--- Baba/Views/NewGameView.cs
+++ Baba/Views/NewGameView.cs
@@ -96,6 +96,14 @@ namespace Baba.Views
             killSystem.Reset();
             sinkSystem.Reset();
             winSystem.Reset();
+          
+
+            transforms = gridMaker.MakeGrid(level[0]);
+            ruleSystem.UpdateRules();
+            state = State.Play;
+        }
+        public void resetControls()
+        {
             m_inputKeyboard.resetCommands();
             m_inputKeyboard.registerCommand(Keys.Escape, true, new InputDeviceHelper.CommandDelegate(Escape));
             m_inputKeyboard.registerCommand(controls.Controls[0], true, new InputDeviceHelper.CommandDelegate(moveUp));
@@ -104,10 +112,6 @@ namespace Baba.Views
             m_inputKeyboard.registerCommand(controls.Controls[3], true, new InputDeviceHelper.CommandDelegate(moveRight));
             m_inputKeyboard.registerCommand(controls.Controls[4], true, new InputDeviceHelper.CommandDelegate(ResetKeyPress));
             m_inputKeyboard.registerCommand(controls.Controls[5], true, new InputDeviceHelper.CommandDelegate(Undo));
-
-            transforms = gridMaker.MakeGrid(level[0]);
-            ruleSystem.UpdateRules();
-            state = State.Play;
         }
         public override void update(GameTime gameTime)
         {
